433a0312532ededd6f1c034a5b83b0e9e02822de
Victor <vkseong@gmail.com>
2026-09-09 09:42:16 +0200

    Validate chessboard size input

--- Chessboard/Program.cs
+++ Chessboard/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Mitt schackbräde");
+Console.WriteLine("------------------------------------\n");
+
+
+bool isValidInput = false;
+int boardSize = 0;
+
+while (!isValidInput)
+{
+    Console.Write("Ange brädets storlek (3-50): ");
+    string input = Console.ReadLine();
+
+    if (int.TryParse(input, out int size))
+    {
+        if (size >= 3 && size <= 50)
+        {
+            Console.WriteLine($"Lyckades! Nya brädstorlek är {size}.");
+            isValidInput = true;
+            boardSize = size;
+        }
+        else
+        {
+            Console.WriteLine("Fel: Talet måste vara mellan 3 och 50. Försök igen!");
+        }
+
+    }
+    else
+    {
+        Console.WriteLine("Fel: Ogiltig inmatning. Försök igen! Vänligen ange ett heltal.");
+    }
+}
