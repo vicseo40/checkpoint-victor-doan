7b6865ccc756af6b68a90bee517a2068e99b96f5
vicseo40 <99327487+vicseo40@users.noreply.github.com>
2026-09-23 11:47:04 +0200

    Updated search method

--- Chessboard/Search.cs
+++ Chessboard/Search.cs
@@ -8,9 +8,11 @@ namespace Chessboard
     public class Search
     {
         public List<ChessBoard> AllSavedBoards {  get; set; }
+        public ValidateInput Input {  get; set; }
 
         public Search()
         {
+            Input = new ValidateInput();
             AllSavedBoards = new List<ChessBoard>();
 
             string filePath = "Owners.json";
@@ -19,23 +21,40 @@ namespace Chessboard
             {
                 string existingFile = File.ReadAllText(filePath);
                 AllSavedBoards = JsonConvert.DeserializeObject<List<ChessBoard>>(existingFile);
-            }
-
-            for (int i = 0; i < AllSavedBoards.Count; i++) 
-            {
-                string ownerName = AllSavedBoards[i].Owner;
-                int size = AllSavedBoards[i].BoardSize;
-                Console.WriteLine($"Owner name is: {ownerName}. Board size is: {size}");
+                FindSingleOwner();
             }
         }
 
         public void FindSingleOwner()
         {
-            
+            Console.Write("Vem vill du hitta: ");
+            string nameInput = Console.ReadLine();
+            string owner = nameInput;
+
+            for (int i = 0; i < AllSavedBoards.Count; i++)
+            {
+                if (owner == AllSavedBoards[i].Owner)
+                {
+                    string ownerName = AllSavedBoards[i].Owner;
+                    int size = AllSavedBoards[i].BoardSize;
+                    Console.WriteLine($"Owner exists!");
+                    Console.WriteLine($"Owner name is: {ownerName}. Board size is: {size}");
+                }
+                else
+                {
+                    Console.WriteLine($"Owner does not exist!");
+                }
+            }
         }
 
         public void FindAllOwners() 
         {
+            for (int i = 0; i < AllSavedBoards.Count; i++)
+            {
+                string ownerName = AllSavedBoards[i].Owner;
+                int size = AllSavedBoards[i].BoardSize;
+                Console.WriteLine($"Owner name is: {ownerName}. Board size is: {size}");
+            }
         }
 
 
