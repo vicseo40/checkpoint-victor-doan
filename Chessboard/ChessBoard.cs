cdda317b177b16d2081a9b1f4fc55528563b2393
vicseo40 <99327487+vicseo40@users.noreply.github.com>
2026-09-23 10:48:28 +0200

    Multiple Json objects can be stored

--- Chessboard/ChessBoard.cs
+++ Chessboard/ChessBoard.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -47,6 +48,23 @@ namespace Chessboard
 
                 }
             }
+        }
+
+        public static void SaveChessBoard(ChessBoard board)
+        {
+            // Convert till Json
+            List<ChessBoard> allBoards = new List<ChessBoard>();
+            string filePath = "Owners.json";
+
+            if (File.Exists(filePath))
+            {
+                string existingFile = File.ReadAllText(filePath);
+                allBoards = JsonConvert.DeserializeObject<List<ChessBoard>>(existingFile);
+            }
+
+            allBoards.Add(board);
+            string json = JsonConvert.SerializeObject(allBoards, Formatting.Indented);
+            File.WriteAllText(filePath, json);
         }
     }
 }
--- Chessboard/Program.cs
+++ Chessboard/Program.cs
@@ -9,24 +9,11 @@ static void CreateChessBoard()
     // Skapa och köra object
     ChessBoard board = new ChessBoard(3, 50);
     board.BuildChessBoard();
-
-    // Convert till Json
-<<<<<<< Updated upstream
-    List<ChessBoard> allBoards = new List<ChessBoard>();
-    string filePath = "Owners.json";
-
-    if (File.Exists(filePath))
-    {
-        string existingJson = File.ReadAllText(filePath);
-    }
-=======
-    string json = JsonConvert.SerializeObject(board, Formatting.Indented);
-    File.WriteAllText("Owner.json", json);
->>>>>>> Stashed changes
-
+    ChessBoard.SaveChessBoard(board);
 }
 
+
+
+
+
 CreateChessBoard();
-
-
-
